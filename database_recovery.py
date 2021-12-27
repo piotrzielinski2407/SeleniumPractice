151b9f668cd2c39854920ad3c516116bc7e708e2
Piotrek_Z <piotrzielinski2407@gmail.com>
2021-12-27 13:44:50 +0100

    Sql functionality added, tables in DB created

---
+++ database_recovery.py
@@ -0,0 +1,107 @@
+'''
+THIS FILE SHOULD BE CALLED ONLY WHEN NEW, EMPTY DATABASE IS CREATED AND TABLES STRUCTURE NEEDS TO BE RECOVERED
+'''
+table1_name = 'leagues'
+table1_cols=[
+                ['id', 'SERIAL PRIMARY KEY'],
+                ['league_ticker', 'varchar(5)', 'UNIQUE NOT NULL'],
+                ['league_full_name', 'varchar(50)', 'UNIQUE NOT NULL'],
+                ['league_hyperlink', 'varchar(100)', 'NOT NULL']
+            ]
+
+table2_name = 'teams'
+table2_cols=[
+                ['id', 'SERIAL PRIMARY KEY'],
+                ['league_full_name', 'varchar(50)', 'NOT NULL REFERENCES leagues(league_full_name)'],
+                ['league_ticker', 'varchar(5)', 'NOT NULL REFERENCES leagues(league_ticker)'],
+                ['team_name', 'varchar(80)', 'UNIQUE NOT NULL'],
+                ['team_ticker', 'varchar(5)', 'UNIQUE NOT NULL']
+            ]
+
+table3_name = 'matches'
+table3_cols=[
+                ['id', 'SERIAL PRIMARY KEY'],
+                ['id_flashscore_match', 'varchar(20)', 'UNIQUE NOT NULL'],
+                ['league_ticker', 'varchar(5)', 'NOT NULL REFERENCES leagues(league_ticker)'],
+                ['match_date', 'date', 'NOT NULL'],
+                ['match_time', 'time', 'NOT NULL'],
+                ['home_team_ticker', 'varchar(5)', 'NOT NULL REFERENCES teams(team_ticker)'],
+                ['away_team_ticker', 'varchar(5)', 'NOT NULL REFERENCES teams(team_ticker)'],
+                ['hyperlink', 'varchar(100)', 'NOT NULL'],
+                ['ht_q1_pts', 'int', 'NOT NULL'],
+                ['at_q1_pts', 'int', 'NOT NULL'],
+                ['ht_q2_pts', 'int', 'NOT NULL'],
+                ['at_q2_pts', 'int', 'NOT NULL'],
+                ['ht_q3_pts', 'int', 'NOT NULL'],
+                ['at_q3_pts', 'int', 'NOT NULL'],
+                ['ht_q4_pts', 'int', 'NOT NULL'],
+                ['at_q4_pts', 'int', 'NOT NULL'],
+                ['ht_q5_pts', 'int'],
+                ['at_q5_pts', 'int'],
+                ['ht_odds_start', 'real'],
+                ['at_odds_start', 'real'],
+                ['ht_odds_end', 'real'],
+                ['at_odds_end', 'real']
+            ]
+
+table4_name = 'players_info'
+table4_cols=[
+                ['id', 'SERIAL PRIMARY KEY'],
+                ['id_flashscore_player', 'varchar(20)', 'UNIQUE NOT NULL'],
+                ['full_name', 'varchar(50)', 'NOT NULL'],
+                ['birth_date', 'date', 'NOT NULL'],
+                ['height', 'int', 'NOT NULL'],
+                ['hyperlink', 'varchar(80)', 'NOT NULL']
+            ]
+
+table5_name = 'players_statistic'
+table5_cols=[
+                ['id', 'SERIAL PRIMARY KEY'],
+                ['id_match', 'int', 'REFERENCES matches(id)'],
+                ['id_flashscore_match', 'varchar(20)', 'NOT NULL REFERENCES matches(id_flashscore_match)'],
+                ['id_player', 'int', 'NOT NULL REFERENCES players_info(id)'],
+                ['name_short', 'varchar(20)', 'NOT NULL'],
+                ['team_ticker', 'varchar(5)', 'NOT NULL REFERENCES teams(team_ticker)'],
+                ['pts', 'int', 'NOT NULL'],
+                ['reb', 'int', 'NOT NULL'],
+                ['ast', 'int', 'NOT NULL'],
+                ['min', 'int', 'NOT NULL'],
+                ['fgm', 'int', 'NOT NULL'],
+                ['fga', 'int', 'NOT NULL'],
+                ['pm2', 'int', 'NOT NULL'],
+                ['pa2', 'int', 'NOT NULL'],
+                ['pm3', 'int', 'NOT NULL'],
+                ['pa3', 'int', 'NOT NULL'],
+                ['ftm', 'int', 'NOT NULL'],
+                ['fta', 'int', 'NOT NULL'],
+                ['plus_minus', 'int', 'NOT NULL'],
+                ['ofrb', 'int', 'NOT NULL'],
+                ['dfrb', 'int', 'NOT NULL'],
+                ['pf', 'int', 'NOT NULL'],
+                ['st', 'int', 'NOT NULL'],
+                ['tovr', 'int', 'NOT NULL'],
+                ['bs', 'int', 'NOT NULL'],
+                ['ba', 'int', 'NOT NULL'],
+                ['tfs', 'int', 'NOT NULL']
+            ]
+
+table6_name = 'player_carrer'
+table6_cols=[
+                ['id', 'SERIAL PRIMARY KEY'],
+                ['id_player', 'int', 'NOT NULL REFERENCES players_info(id)'],
+                ['id_flashscore_player',' varchar(20)', 'NOT NULL REFERENCES players_info(id_flashscore_player)'],
+                ['season_start_year', 'int', 'NOT NULL'],
+                ['season_end_year', 'int', 'NOT NULL'],
+                ['team_ticker', 'varchar(5)', 'NOT NULL REFERENCES teams(team_ticker)'],
+                ['league_ticker', 'varchar(5)', 'NOT NULL REFERENCES leagues(league_ticker)']
+            ]
+
+from sql_postgress_comms import DB
+
+with DB() as db:
+    db.create_table(table1_name, table1_cols)
+    db.create_table(table2_name, table2_cols)
+    db.create_table(table3_name, table3_cols)
+    db.create_table(table4_name, table4_cols)
+    db.create_table(table5_name, table5_cols)
+    db.create_table(table6_name, table6_cols)
--- sql_postgress_comms.py
+++ sql_postgress_comms.py
@@ -9,29 +9,80 @@ class DB():
     This class is designed as multipurpouse and universal to all future usage of postgressSQL.
     Many operation will double check if table exist to avoid situation that someone else or other part 
     of software change something in DB
+    Any operation on slected table make it active one
+    Any intance fo this should be used with "with" statement to allow safe connecion close, otherwise, 
+    methods close_cur and close_conn should be called manually
     """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         try:
-            self.conn = psycopg2.connect(database=config["db_name"], user=config["user"], password=config["password"], host=config["host"], port=config["port"])
-            self.cur = self.conn.cursor()
+            self.__conn = psycopg2.connect(database=config["db_name"], user=config["user"], password=config["password"], host=config["host"], port=config["port"])
+            self.__cur = self.__conn.cursor()
             logger_object.log('Connection with DB sucesfull', 'INFO')
         except Exception as e:
             logger_object.log(f'Exception occure during estabilishing connection with DB: {e}', 'ERROR')
         self.active_table = None
+    
+    def __commit_after(method):
+        def wrapper(self, *args, **kwargs):
+            method(self, *args, **kwargs)
+            self.commit()
+        return wrapper
 
-    def querry(self, sql, return_mode = None):
-        '''
-        Method for user to create custom querry to db
-        '''
-        try:
-            self.cur.execute(sql)
-            #return mode missing-should be codded
-            logger_object.log('Custom querry executed properly', 'INFO')
-        except Exception as e:
-            logger_object.log(f'Exception {e} occured during custom querry execution', 'ERROR')
+    def __execute_with_log(method):
+        def wrapper(self, *args, **kwargs):
+            try:
+                result = method(self, *args, **kwargs)
+                logger_object.log(f'Method "{method.__name__}" executed corectly', 'INFO')
+                return result
+            except Exception as e:
+                logger_object.log(f'Exception {e} occured during executing "{method.__name__}"', 'ERROR')
+                return False
+        return wrapper
+    
+    def __table_set(method):
+        def wrapper(self, *args, table_name = None, **kwargs):
+            if table_name is None:
+                if self.is_table_exist() is not False: 
+                    return method(self, *args, **kwargs)
+                return False
 
-    def create_table(self, table_name, column_parameters = None, table_constraints = None):
+            if self.is_table_exist(table_name) is not False:
+                self.active_table = table_name
+                return method(self, *args, **kwargs)
+            return False
+        return wrapper
+
+    @__execute_with_log
+    def execute(self, sql):
+        return self.__cur.execute(sql)   
+    
+    @__execute_with_log
+    def fetchone(self):
+        return self.__cur.fetchone()
+
+    @__execute_with_log
+    def fetchmany(self, rows):
+        return self.__cur.fetchmany(rows)
+
+    @__execute_with_log
+    def fetchall(self):
+        return self.__cur.fetchall()
+
+    @__execute_with_log
+    def close_conn(self):
+        self.__conn.close()
+
+    @__execute_with_log
+    def commit(self):
+        self.__conn.commit()
+        
+    @__execute_with_log
+    def close_cur(self):
+        self.__cur.close()
+
+    @__commit_after
+    def create_table(self, table_name, column_parameters = None):
         '''
         Method that will create table with columns in it only if column_parameters is provided, otherwise only empty table is created.
         Make that table active one.
@@ -39,110 +90,89 @@ class DB():
         '''
         if not self.is_table_exist(table_name):
             sql = f"CREATE TABLE {table_name} ();"
-            self.cur.execute(sql)
+            self.execute(sql)
             self.active_table = table_name
             logger_object.log(f'Table "{table_name}" created', 'DEBUG')
-            #still column creation should be codded
+            if column_parameters is not None:
+                return self.add_columns(column_parameters, table_name = self.active_table)
             return True
         logger_object.log(f'Table with that name:"{table_name}" already exist or no table name provided', 'WARNING')
         return False
 
+    @__commit_after
+    @__table_set
     def rename_table(self, new_table_name, table_name = None):
         '''
         Method that will change table name for other, if no table_name provided, active on will be taken into consideration
         Table with new name become active one
         Return True if everything passed withoud errors
         '''
-        result = self.__table_name_set(table_name)#checking if active or provided table exist
-        if result is not False:
-            table_name = result
-        else:
-            return False
-
-        if not self.is_table_exist(new_table_name):#chec if new table name is availible
-            sql = f"ALTER TABLE {table_name} RENAME TO {new_table_name};"
-            self.cur.execute(sql)
-            logger_object.log(f'Sucess {table_name} changed name to {new_table_name} and become active', 'INFO')
+        if not self.is_table_exist(new_table_name):#check if new table name is availible
+            sql = f"ALTER TABLE {self.active_table} RENAME TO {new_table_name};"
+            self.execute(sql)
+            logger_object.log(f'Sucess {self.active_table} changed name to {new_table_name} and become active', 'DEBUG')
             self.active_table = new_table_name
             return True
-        logger_object.log(f'Unable to changed name of {table_name} to {new_table_name}, {new_table_name} is already used ', 'ERROR')
+        logger_object.log(f'Unable to changed name of {self.active_table} to {new_table_name}, {new_table_name} is already used ', 'ERROR')
         return False 
 
+    @__commit_after
+    @__table_set     
+    def drop_table(self, table_name = None):
+        '''
+        Method that will drop table table_name if no table_name is provided, active table is taken into consideration
+        '''
+        sql = f'DROP TABLE {self.active_table}'
+        self.execute(sql)
+        return True
+
+    @__table_set
     def add_columns(self, columns_parameters, table_name = None):
         '''
         Method that will add columns to table, if no table_name is provided current table is taken into consideration.
         columns variable should be list of 3 elements lists: 1el-column name,   2el-datatype, 3el-column constraint.
         '''
-        result = self.__table_name_set(table_name)#checking if active or provided table exist
-        logger_object.log(f'TEST {result}', 'DEBUG')
-        if result is not False:
-            table_name = result
-        else:
-            logger_object.log(f'Table {table_name} not found in DB', 'DEBUG')
-            return False
-        
+        return self.__column_operation('ADD', columns_parameters)
+    
+    @__table_set
+    def remove_columns(self, columns_names, table_name = None):
+        '''
+        Method that will remove column/s from table 
+        Column names should be list of lists to proper functioning
+        '''
+        return self.__column_operation('DROP', columns_names)
+    
+    @__commit_after
+    def __column_operation(self, opearation, columns_parameters):
+        '''
+        Universal method to operate on columns
+        '''
         if isinstance(columns_parameters, list) and isinstance(columns_parameters[0], list):
-            sql_command = self.__sql_command_add_column(columns_parameters, table_name)
+            sql_command = self.__sql__column_operation(opearation, columns_parameters, self.active_table)
             if sql_command is not False:
                 sql = sql_command
             else:
                 return False#exception already raised in __sql_command_add_column
-            try:
-                self.cur.execute(sql)
-            except Exception as e:
-                logger_object.log(f'Exception: {e} occure durign adding columns', 'ERROR')
-                return False
-            logger_object.log('Columns added', 'DEBUG')
-            return True
+            
+            if self.execute(sql) is not False:
+                logger_object.log('Columns added', 'DEBUG')
+                return True
+            return False
         logger_object.log('Column parametrs should be type of list', 'ERROR')
-        return False        
-    
-    def drop_table(self, table_name = None):
-        '''
-        Method that will drop table table_name if no table_name is provided, active table is taken into consideration
-        '''
-        result = self.__table_name_set(table_name)#checking if active or provided table exist
-        if result is not False:
-            table_name = result
-        else:
-            return False
-        sql = f'DROP TABLE {table_name}'
-        try:
-            self.cur.execute(sql)
-            logger_object.log(f'Table {table_name} sucefully deleted', 'INFO')
-            return True
-        except Exception as e:
-            logger_object.log(f'Exception: {e} occured durign deleting table: {table_name}', 'INFO')
-            return False
+        return False   
 
-
-    def __sql_command_add_column(self, columns_parameters, table_name):
+    @__execute_with_log
+    def __sql__column_operation(self, opearation, columns_parameters, table_name):
+        '''
+        Column operation like: ADD, DROP, ALTER
+        '''
         sql = f'ALTER TABLE {table_name}'
-        try:
-            for column_param in columns_parameters:
-                sql = sql + \
-                f' ADD COLUMN {column_param[0]} {column_param[1]} {column_param[2]},'
-            return sql[:len(sql) - 1] + ';'
-        except Exception as e:
-            logger_object.log(f'Exception: {e} occured during creating sql command ', 'ERROR')
-            return False
-
-    def __table_name_set(self, table_name):
-        '''
-        Inside method only to check if provided table name exist in db, if not active table is checked
-        Return correct table name if it is true, return false and log error if not.
-        '''
-        if table_name is None:
-            if self.is_table_exist() is not False: 
-                return self.active_table
-            return False
-
-        if self.is_table_exist(table_name) is not False:
-            return table_name
-        return False
-
-    def activate_table(self, table_name):
-        pass
+        for column_param in columns_parameters:
+            sql = sql + f' {opearation} COLUMN'
+            for param in column_param:
+                sql = sql + f' {param}'
+            sql = sql + ','
+        return sql[:len(sql) - 1] + ';'
 
     def is_table_exist(self, table_name=None):
         '''
@@ -155,24 +185,18 @@ class DB():
             else:
                 logger_object.log(f'Active table is not set, no table to check if exist' 'ERROR')
                 return False
-        self.cur.execute(f"SELECT EXISTS(SELECT relname FROM pg_class WHERE relname = '{table_name}');")
-        result = self.cur.fetchone()[0]
+        self.execute(f"SELECT EXISTS(SELECT relname FROM pg_class WHERE relname = '{table_name}');")
+        result = self.fetchone()[0]
         logger_object.log(f'Execute test if {table_name} in DB with result {result}', 'DEBUG')
         return result
+    
+    def __enter__(self):
+        return self
 
-    def __exit__(self):
-
-        try:
-            self.cur.close()
-            logger_object.log('DB cursor closed sucesfull', 'INFO')
-        except Exception as e:
-            logger_object.log(f'Exception occure during closing DB cursor: {e}', 'ERROR')
-
-        try:
-            self.conn.close()
-            logger_object.log('DB connection closed sucesfull', 'INFO')
-        except Exception as e:
-            logger_object.log(f'Exception occure during closing DB connection: {e}', 'ERROR')
+    def __exit__(self, *args, **kwargs):
+        self.close_cur()
+        self.close_conn()
+        logger_object.log(f'Connection with DB ended', 'INFO')
         
     def __str__(self):
         return 'Connection with DB provider'
@@ -180,21 +204,10 @@ class DB():
     def __repr__(self):
         return 'DB()'
 
-    def __exit__(self):
-        try:
-            self.conn.commit()
-        except:
-            pass
-        try:
-            self.conn.close()
-        except:
-            pass
-
-
 if __name__ == '__main__':
     #some testing here  
-    table_name = 'test3'
-    db = DB()
+    table_name2 = 'test48'
+    
 
     column_parameters = [
                         ['id', 'SERIAL', 'PRIMARY KEY'],
@@ -202,7 +215,9 @@ if __name__ == '__main__':
                         ['column_3', 'varchar(10)', 'NOT NULL']
                         ]
 
-    db.rename_table('test5','test3')
+    with DB() as db:
+        db.active_table = table_name2
+        db.create_table('test50',column_parameters)
 
 
 
